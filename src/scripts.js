ee18588bad7d22d12f35477a3e2e79003a0e5499
Aubrey Quinn <aquinn@softworks.com>
2020-11-01 21:13:24 +0000

    in progress

--- src/scripts.js
+++ src/scripts.js
@@ -12,15 +12,26 @@ function flipCard() {
     }
 }
 
-function buttonGlow() {
-    //find the Ts&Cs button and add a glow
-    //find the back button and make it glow
-    // var element = document.getElementById('flip-card-inner');
-    // var flip_class = 'flip-180';
-    // //if it has the 180 class, remove it, if it doesn't have it, then add it
-    // if (element.classList.contains(flip_class)) {
-    //     element.classList.remove(flip_class);
-    // } else {
-    //     element.classList.add(flip_class);
-    // }
+function tCButtonGlow() {
+    var element = document.getElementById('T&C');
+
+    var glow_class = 'glow';
+
+    if (element.classList.contains(glow_class)) {
+        element.classList.remove(glow_class);
+    } else {
+        element.classList.add(glow_class);
+    }
+}
+
+function backButtonGlow() {
+    var element = document.getElementById('back');
+
+    var glow_class = 'glow';
+
+    if (element.classList.contains(glow_class)) {
+        element.classList.remove(glow_class);
+    } else {
+        element.classList.add(glow_class);
+    }
 }
